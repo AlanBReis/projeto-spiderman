5f403a8924392296069c67b6e5f9076da0cf4c95
Alan Reis <140574199+AlanBReis@users.noreply.github.com>
2024-01-27 02:59:35 -0300

    commit js

--- assets/scripts/card-hover-animation.js
+++
@@ -1,6 +0,0 @@
-function addEventListenersToCards() {
-    const cardElements = document.getElementsByClassName("s-card");
-    console.log(cardElements);
-}
-
-document.addEventListener("DOMContentLoaded", addEventListenersToCards, false);
