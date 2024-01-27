6501f557f9415e0f8fd3c90f58df6f16fa39ae5f
Alan Reis <140574199+AlanBReis@users.noreply.github.com>
2024-01-27 02:49:54 -0300

    estilizando css

---
+++ assets/scripts/card-hover-animation.js
@@ -0,0 +1,6 @@
+function addEventListenersToCards() {
+    const cardElements = document.getElementsByClassName("s-card");
+    console.log(cardElements);
+}
+
+document.addEventListener("DOMContentLoaded", addEventListenersToCards, false);
